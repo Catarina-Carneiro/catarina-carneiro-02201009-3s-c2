ba750b817e7d2314b98695b1e56a7e7af76ff092
Catarina-Carneiro <catarina.carneiro@bandtec.com.br>
2021-04-16 16:37:06 -0300

    iniciando

---
+++ continuada2/src/main/java/com/example/continuada2/Continuada2Application.java
@@ -0,0 +1,13 @@
+package com.example.continuada2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Continuada2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Continuada2Application.class, args);
+	}
+
+}
---
+++ continuada2/src/test/java/com/example/continuada2/Continuada2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.continuada2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Continuada2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
